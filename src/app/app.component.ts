43258b23ce77d0773563e2c679c4605ae6b5608a
M-Riggs <michael@michael-riggs.com>
2018-11-08 16:32:42 -0600

    Working on email functionality of the contact form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from "@angular/platform-browser";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,30 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public name = '';
+  public email = '';
+  public subject = '';
+  public message = '';
+  
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private http: HttpClient
   ) {
       this.matIconRegistry.addSvgIcon('headshot', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/headshot.svg"));
       this.matIconRegistry.addSvgIcon('linkedin', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/linkedinLogo.svg"));
   }
 
   title = 'Michael Riggs';
+
+  sendEmail() {
+    let postVars = {
+      email : this.email,
+      name : this.name,
+      message : this.message,
+      subject: this.subject
+    };
+
+    this.http.post("http://michael-riggs.com/email.php", postVars).subscribe();
+  }
 }
